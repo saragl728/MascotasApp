a73eecd98268d584cf7b38251b60ff0fe311f233
Usuario <saragl728@gmail.com>
2023-12-13 10:02:19 +0100

    Añado las búsquedas de perros y gatos de los dueños

--- app/src/main/java/com/example/mascotasapp/database/Dao.kt
+++ app/src/main/java/com/example/mascotasapp/database/Dao.kt
@@ -2,10 +2,17 @@ package com.example.mascotasapp.database
 
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 
 interface Dao {
 
+    //búsquedas de los números de perros y gatos de los dueños
+    @Query("SELECT COUNT(*) FROM mascotas WHERE esPerro = FALSE AND duenio like :nombre")
+    fun gatosDePropietarios(nombre:String): Int
+
+    @Query("SELECT COUNT(*) FROM mascotas WHERE esPerro = TRUE AND duenio like :nombre")
+    fun perrosDePropietarios(nombre:String): Int
     @Insert
     fun insertarPropietario(propietarios: Propietarios)
 
