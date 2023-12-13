944ad48191ca48c9210c05383c12dc468ff2d404
Usuario <saragl728@gmail.com>
2023-12-13 08:41:06 +0100

    Inicializo añadiendo el archivo xml del menú

---
+++ app/src/main/java/com/example/mascotasapp/DeleteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mascotasapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class DeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete)
+    }
+}
